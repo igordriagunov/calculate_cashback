dea290f337f3289349d760d2ba17af0faba4be8c
igor <work7x7@yandex.ru>
2018-12-19 10:52:42 +0300

    Doctest added, PASSED

--- app/calculate_cashback.py
+++ app/calculate_cashback.py
@@ -1,5 +1,22 @@
 def calculate_cashback(purchase_amount, purchase_category):
-
+    """
+    >>> calculate_cashback(0, 0)
+    0
+    >>> calculate_cashback(1_000, 0)
+    0
+    >>> calculate_cashback(0, 1)
+    0.0
+    >>> calculate_cashback(5_000, 1)
+    50.0
+    >>> calculate_cashback(300_001, 1)
+    3000
+    >>> calculate_cashback(5_000, 2)
+    250.0
+    >>> calculate_cashback(60_005, 2)
+    3000
+    >>> calculate_cashback(10_004, 3)
+    3000
+    """
     standart_cashback = 0.01
     upper_cashback = 0.05
     special_cashback = 0.3
