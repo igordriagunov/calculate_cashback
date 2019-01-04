41e81008bfaaa0e507375876fad6f3b90f136bd6
igor <work7x7@yandex.ru>
2019-01-04 16:13:12 +0300

    code refactor

--- app/calculate_cashback.py
+++ app/calculate_cashback.py
@@ -1,4 +1,4 @@
-def calculate_cashback(purchase_amount, purchase_category):
+def calculate_cashback(amount, category):
     """
     >>> calculate_cashback(0, 0)
     0
@@ -21,32 +21,29 @@ def calculate_cashback(purchase_amount, purchase_category):
     upper_cashback = 0.05
     special_cashback = 0.3
 
-    standart_buying_category = 1
-    upper_buying_category = 2
-    special_buying_category = 3
+    standart_category = 1
+    upper_category = 2
+    special_category = 3
 
     limit = 3_000
 
-    if purchase_category == standart_buying_category:
-        result = purchase_amount * standart_cashback
-        if result >= limit:
-            return limit
-        return result
+    if category == standart_category:
+        result = amount * standart_cashback
+        return result_or_limit(result, limit)
 
-    if purchase_category == upper_buying_category:
-        result = purchase_amount * upper_cashback
-        if result >= limit:
-            return limit
-        return result
+    if category == upper_category:
+        result = amount * upper_cashback
+        return result_or_limit(result, limit)
 
-    if purchase_category == special_buying_category:
-        result = purchase_amount * special_cashback
-        if result >= limit:
-            return limit
-        return result
+    if category == special_category:
+        result = amount * special_cashback
+        return result_or_limit(result, limit)
 
     else:
         return 0
 
 
-print(calculate_cashback(50000, 1))
+def result_or_limit(result, limit):
+    if result >= limit:
+        return limit
+    return result
